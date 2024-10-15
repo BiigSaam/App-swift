2af363166de71fa8b2334470cb59a32e41974ee0
BiigSaam <absamad938@gmail.com>
2024-10-15 13:56:23 +0200

    Initial Commit

---
+++ App-swift/App_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_swiftApp.swift
+//  App-swift
+//
+//  Created by ABDUL Samad on 15/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct App_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
